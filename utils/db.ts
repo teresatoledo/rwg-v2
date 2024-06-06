c8507b22396953644d195bb0424604ffc2af1709
Teresa <teresatoledolara@gmail.com>
2024-06-06 13:00:46 +0200

    DB

---
+++ utils/db.ts
@@ -0,0 +1,33 @@
+
+import mysql from 'mysql2/promise';
+import jwt from 'jsonwebtoken'
+require('dotenv').config();
+
+export async function connectToDatabase() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: "2P!&$98&8a2kde6",
+    database: process.env.DB_NAME,
+  });
+  await connection.connect();
+  return {db: connection};
+}
+export const generateToken = (payload: any) => {
+  if (!process.env.KEY) {
+    throw new Error("Environment variable KEY is not defined");
+  }
+  const token = jwt.sign(payload, process.env.KEY, {expiresIn: '1h'});
+  return token;
+}
+export const verifyToken = (token: any) => {
+  if (!process.env.KEY) {
+    throw new Error("Environment variable KEY is not defined");
+  }
+  try {
+    const verifyTkn = jwt.verify(token, process.env.KEY);
+    return verifyTkn;
+  } catch (error) {
+    return null;
+  }
+}
